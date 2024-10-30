6c5944fe25a1ed5ec778db30d6d030bb56bd7ae1
hiitsvan <vanessa.yeo.2023@smu.edu.sg>
2024-10-30 15:16:39 +0800

    firebase

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,11 @@
 // import router from './routers'
 // createApp(App).user(router).mount('#app')
 import { createApp } from 'vue';
-import App from './App.vue';
+import App from './App.vue'; // .default is necessary for default exports
 import router from './routers'; // Assuming you have router setup
 import store from './store'; // Import the Vuex store
-import './assets/styles/custom.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap'; // Optional: This imports Bootstrap's JavaScript
+import 'bootstrap'; 
 
 
 
